48e7a7d814bea2fc7f4de0861df5ecbef7d90092
Никита Рожков <admin@admin.local>
2024-09-07 22:16:47 +0300

    changing the admin panel and improving unit-tests

--- nof_web/nucleus/plugins/test.py
+++ nof_web/nucleus/plugins/test.py
@@ -9,15 +9,18 @@ from nucleus.models import Post, Post_Games, User
 
 def create_data_from_bd():
     hash_pwd = generate_password_hash('admin')
-    new_user = User(login='admin', password=hash_pwd, role='admin')
-    post = Post(title='TEST', short_body='TEST', full_body='TEST', img='', status='Внешнее мероприятие')
-    game = Post_Games(title='TEST', body='TEST', img='')
+    hash_pwd2 = generate_password_hash('test')
+    new_user = User(login='admin', password=hash_pwd, role='admin', first_name='Никита', last_name='Рожков', middle_name='Игоревич', faculty='ФПСОИБ', course='3', rating=100)
+    new_user2 = User(login='test', password=hash_pwd2, role='user', first_name='Станислав', last_name='Помещиков', middle_name='Евгеньевич', faculty='ФПСОИБ', course='2', rating='0')
+    post = Post(title='TEST', short_body='TEST', full_body='TEST', img='../static/images/news/pink-red-nebula-space-cosmos-4k-7m-3840x2400.jpg', status='Внешнее мероприятие')
+    game = Post_Games(title='TEST', body='TEST', img='../static/images/games/pink-red-nebula-space-cosmos-4k-7m-3840x2400.jpg')
 
     User.query.delete()
     Post.query.delete()
     Post_Games.query.delete()
 
     db.session.add(new_user)
+    db.session.add(new_user2)
     db.session.add(post)
     db.session.add(game)
     db.session.commit()
